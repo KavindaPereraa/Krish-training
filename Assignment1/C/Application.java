bb1cfb86d5cfc0a4d8e182b3d7e70dd9293a2c72
Subadhra Kankanamalage Kavinda Gayeshan Perera <98266212+Kavinda401@users.noreply.github.com>
2022-03-01 15:47:31 +0530

    Update Application.java

--- Assignment1/C/Application.java
+++ Assignment1/C/Application.java
@@ -1,10 +1,10 @@
 package c;
-
+//importing the packages / classes
 import b.B;
 import a.A;
  
 public class Application{
-	public static void main(String[] args)
+	public static void main(String[] args) //main method 
 	{
 		B newObj = new B(); //creating an object from the b class
 		newObj.run();	//calling the method in the b class
