eba855bdae4c483f764057c57385b820382eba54
Subadhra Kankanamalage Kavinda Gayeshan Perera <98266212+Kavinda401@users.noreply.github.com>
2022-03-01 15:43:54 +0530

    Update A.java

--- Assignment1/A/A.java
+++ Assignment1/A/A.java
@@ -2,7 +2,7 @@ package a; //declaring a package
 
 public class A{
 	public void run(){
-		System.out.println(" A directory is running"); //non return method 
+		System.out.println(" A directory is running"); //non return method void pritning A directory is running
 	}
 	
 
