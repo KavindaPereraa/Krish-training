d85bd2a1699bbc26622e4d8a3d9246e7f58d62b5
Subadhra Kankanamalage Kavinda Gayeshan Perera <98266212+Kavinda401@users.noreply.github.com>
2022-03-06 17:55:07 -0800

    Add files via upload

---
+++ src/main/java/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+
+   public static void main(String [] args) throws InterruptedException {
+       NumberofOccurences s1 = new NumberofOccurences(); //create an object of class
+       s1.getingInput();    //call the method
+
+
+   }
+}
+
+
+
+
+
