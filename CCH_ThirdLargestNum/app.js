6621dafb8712efc3d84fd252472eb7975710d879
Subadhra Kankanamalage Kavinda Gayeshan Perera <98266212+KavindaPereraa@users.noreply.github.com>
2022-03-10 18:57:24 -0800

    Update app.js

--- CCH_ThirdLargestNum/app.js
+++ CCH_ThirdLargestNum/app.js
@@ -10,17 +10,17 @@ const thirdLargestNumber= require('./thirdLargestNumber');
     
     let word_Split = thirdLargestNumber.splitWord(wordA); //spliting the words
     
-    let validate_User = thirdLargestNumber.validateUserInput(word_Split);
-     
-    if(validate_User=="Done"){
+    let validate_User = thirdLargestNumber.validateUserInput(word_Split); //calling the validate user function 
+     //sucesfull then we sort numbers and print the third largest
+    if(validate_User=="Done"){ 
+ 
 
+      let intNumbers = thirdLargestNumber.convertNumberToInt(word_Split); //convert numbers to integer
 
-      let intNumbers = thirdLargestNumber.convertNumberToInt(word_Split);
-
-      let sortnumbers = thirdLargestNumber.sortNumbers(intNumbers);
+      let sortnumbers = thirdLargestNumber.sortNumbers(intNumbers); //sort the numbers
       console.log("\r\nSorted Numbers : " +sortnumbers)
 
-      let findThirdLargestNumberz = thirdLargestNumber.findThirdLargestNumber(sortnumbers); 
+      let findThirdLargestNumberz = thirdLargestNumber.findThirdLargestNumber(sortnumbers);  //find the third largest number
       console.log("\r\nThrid Largest Number : "+findThirdLargestNumberz);  
     }else{
       console.log(validate_User);
