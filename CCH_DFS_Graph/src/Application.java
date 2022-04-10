6e5c31609d17572b6df22ab2b066686e1339c88c
Subadhra Kankanamalage Kavinda Gayeshan Perera <98266212+KavindaPereraa@users.noreply.github.com>
2022-04-10 22:33:47 +0530

    Update Application.java

--- CCH_DFS_Graph/src/Application.java
+++ CCH_DFS_Graph/src/Application.java
@@ -1,7 +1,7 @@
 public class Application {
 
     public static void main(String [] args){
-        DFS_Graph dfs_graph = new DFS_Graph();
+        DFS_Graph dfs_graph = new DFS_Graph(); //creating an object of the dfs graph and running it 
         dfs_graph.DFS();
     }
 }
